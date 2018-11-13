fcf8f10b91376b659a6ad3dc6d432d2cd39f6723
Ivana <idukic.hr@gmail.com>
2018-11-13 15:10:02 +0000

    Test hero component.

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
 import { HeroComponent } from './hero.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('HeroComponent (shallow)', () => {
 
@@ -10,7 +12,8 @@ describe('HeroComponent (shallow)', () => {
         // TestBed - can test component and its template running together
         // create module for testing
         TestBed.configureTestingModule({
-            declarations: [HeroComponent] // add component that you are testing
+            declarations: [HeroComponent], // add component that you are testing
+            schemas: [NO_ERRORS_SCHEMA]
         });
 
 
@@ -23,5 +26,17 @@ describe('HeroComponent (shallow)', () => {
     it('should have the correct hero', () => {
         fixture.componentInstance.hero = { id: 1, name: 'Batman', strength: 3 };
         expect(fixture.componentInstance.hero.name).toEqual('Batman');
+    });
+
+    it('should render the hero name in an anchor tag', () => {
+        fixture.componentInstance.hero = { id: 1, name: 'Batman', strength: 3 };
+
+        // prop name do not get updated until change detection runs
+        // we ned to tell Angulr to implement the bindings
+        fixture.detectChanges();
+
+        const deA = fixture.debugElement.query(By.css('a'));
+        expect(deA.nativeElement.textContent).toContain('Batman');
+        // expect(fixture.nativeElement.querySelector('a').textContent).toContain('Batman');
     });
 });
